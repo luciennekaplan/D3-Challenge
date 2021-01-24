778f7d42536a57d6dcb4836feb616de3f8a9fa0a
luciennekaplan <lucykaplan@Lucys-MacBook-Air.local>
2021-01-24 15:23:03 -0800

    Added axis labels

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,13 @@
 // Define SVG area dimensions
-var svgWidth = 800;
+var svgWidth = 960;
 var svgHeight = 600;
 
 //Define chart margins as an object
 var chartMargin = {
     top: 30,
-    right: 30,
-    bottom: 30,
-    left: 30
+    right: 60,
+    bottom: 60,
+    left: 60
 };
 
 // Define dimensions of chart area
@@ -25,44 +25,61 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${chartMargin.right}, ${chartMargin.top})`);
 
 // Access CSV
-d3.csv("assets/data/data.csv").then(function(healthData) {
+d3.csv("assets/data/data.csv").then(function (healthData) {
 
-// Cast the hours value to a number for each piece of tvData
-healthData.forEach(function(d) {
-    d.poverty = +d.poverty;
-    d.healthcare = +d.healthcare;
-  });
+    // Cast the hours value to a number for each piece of tvData
+    healthData.forEach(function (d) {
+        d.poverty = +d.poverty;
+        d.healthcare = +d.healthcare;
+    });
 
 
-// Configure a band scale for the horizontal axis
-var xScale = d3.scaleLinear()
-    .domain([8, d3.max(healthData, d => d.poverty)])
-    .range([0, chartWidth])
+    // Configure a band scale for the horizontal axis
+    var xScale = d3.scaleLinear()
+        .domain([8, d3.max(healthData, d => d.poverty)])
+        .range([0, chartWidth]);
 
-// Create a linear scale for the vertical axis.
-var yScale = d3.scaleLinear()
-    .domain([0, d3.max(healthData, d => d.healthcare)])
-    .range([chartHeight, 0]);
+    // Create a linear scale for the vertical axis.
+    var yScale = d3.scaleLinear()
+        .domain([0, d3.max(healthData, d => d.healthcare)])
+        .range([chartHeight, 0]);
 
-// Create two new functions passing our scales in as arguments
-var bottomAxis = d3.axisBottom(xScale);
-var leftAxis = d3.axisLeft(yScale);
+    // Create two new functions passing our scales in as arguments
+    var bottomAxis = d3.axisBottom(xScale);
+    var leftAxis = d3.axisLeft(yScale);
 
-// Append two SVG group elements to the chartGroup area,
-// and create the bottom and left axes inside of them
-chartGroup.append("g")
-    .call(leftAxis);
+    // Append two SVG group elements to the chartGroup area,
+    // and create the bottom and left axes inside of them
+    chartGroup.append("g")
+        .call(leftAxis);
 
-chartGroup.append("g")
-    .attr("transform", `translate(0, ${chartHeight})`)
-    .call(bottomAxis);
+    chartGroup.append("g")
+        .attr("transform", `translate(0, ${chartHeight})`)
+        .call(bottomAxis);
 
-chartGroup.selectAll("circle")
-    .data(healthData)
-    .enter()
-    .append("circle")
-    .attr("cx", d => xScale(d.poverty))
-    .attr("cy", d => yScale(d.healthcare))
-    .attr("r", "7")
-    .style("fill", "green");
+    chartGroup.selectAll("circle")
+        .data(healthData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xScale(d.poverty))
+        .attr("cy", d => yScale(d.healthcare))
+        .attr("r", "8")
+        .style("fill", "lightblue");
+
+    chartGroup.append("text")
+        .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + chartMargin.top + 10})`)
+        .attr("text-anchor", "middle")
+        .attr("font-size", "20px")
+        .attr("fill", "black")    
+        .text("In Poverty (%)");
+        
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("x", 0-chartHeight/2)
+        .attr("y", 0-chartMargin.right) 
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .attr("fill", "black")
+        .attr("font-size", "20px")
+        .text("Lacks Healthcare (%)"); 
 });
